f550d8b416bd4388490aa85418050d24ffb7289b
Zem <lourdrivera123@gmail.com>
2014-10-28 05:07:39 +0800

    initial commit

---
+++ app/views/backend/default/_layouts/_FooterPartial.blade.php
@@ -0,0 +1,16 @@
+<div id="footer">
+	{{ Setting::value('footer_text') }}
+
+	<div class="span pull-right">
+		<span class="go-top"><i class="icon-arrow-up"></i></span>
+	</div>
+
+	<div class="span pull-right">
+		<a href="{{ url('backend/change_language/es') }}" class="">ES</a>
+	</div>
+
+	<div class="span pull-right">
+		<a href="{{ url('backend/change_language/en') }}" class="">EN</a>
+	</div>
+
+</div>
