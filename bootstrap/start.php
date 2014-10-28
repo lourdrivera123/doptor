891ccd1f66055ae007cbc4c2506464bbd6090ef6
Zem <lourdrivera123@gmail.com>
2014-10-28 09:00:33 +0800

    added a database config for partial hosting

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Paras-PC', 'SUB0DH-PC', 'Bezay', 'monk_', 'universalagency'),
+	'local' => ['Paras-PC', 'SUB0DH-PC', 'Bezay', 'monk_'], 
+	'zem'	=> ['universalagency'],
 
 ));
 
