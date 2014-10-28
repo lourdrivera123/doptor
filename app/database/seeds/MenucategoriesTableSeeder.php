99ff25a544a8cea007150e97bc58d3dde387e2a1
Zem <lourdrivera123@gmail.com>
2014-10-28 10:10:05 +0800

    reinstall doptor

---
+++ app/database/seeds/MenucategoriesTableSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+class MenucategoriesTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		// DB::table('menu_categories')->truncate();
+
+	}
+
+}
